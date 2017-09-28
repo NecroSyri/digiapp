18a5b4a39c4a47f068b521215f54efdd7cd74696
CaptainPeon <cap10peon@gmail.com>
2017-09-28 21:46:18 +0200

    update

--- js/options.js
+++ js/options.js
@@ -1,4 +1,3 @@
-//chrome.tabs.create({url:"pages/options.html"});
 /*
 var valeur=localStorage.getItem(clef);  //récupérer la valeur
 localStorage.setItem(clef,valeur);  //donner une nouvelle valeur
@@ -7,10 +6,9 @@ var clef=localStorage.key(n);  //récupérer la clef du n item.
 localStorage.clear();  //vider localStorage
 localStorage.length();  //le nombre de clef de localStorage
 */
-var username = localStorage.getItem("username");
-if(username == null){
-    console.log("toto");
-    $("#welcomeMessage").hide();
-}else{
-    $(".js_username").html(username);
-}
+$("#validate").click(function(){
+    localStorage.setItem("username",$("#username").val());
+});
+$("#delete").click(function(){
+    localStorage.removeItem("username");
+});
