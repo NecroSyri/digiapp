b34fc1cafb8ec028a34edda7a2883373fcc23610
Crelier <jean-claude.crelier@amersports.com>
2017-10-09 13:35:09 +0200

    jquery test

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+var overlayOn = false;
+var overlayCreated = false;
 chrome.commands.onCommand.addListener(function(command) {
   //alert(command);
   switch(command){
@@ -9,11 +11,30 @@ chrome.commands.onCommand.addListener(function(command) {
 });
 
 function toggle_digivice(){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    $("body").load('../pages/overlay.html');
-    var overlay = $("body").prop('outerHTML');
-    overlay = overlay.replace(/\"/g,"'").replace(/(\r\n|\n|\r)/gm,"");
-    chrome.tabs.insertCSS(tabs[0].id,{file:"../css/overlay.css",allFrames:true});
-    chrome.tabs.executeScript(tabs[0].id,{code:"document.body.innerHTML += \""+overlay+"\""});
-  });
+  if(!overlayCreated){
+    console.log("1");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      $("body").load('../pages/overlay.html');
+      var overlay = $("body").prop('outerHTML');
+      overlay = overlay.replace(/\"/g,"'").replace(/(\r\n|\n|\r)/gm,"");
+      chrome.tabs.executeScript(tabs[0].id,{file:"jquery-3.2.1.min.js"},function(){
+        chrome.tabs.executeScript(tabs[0].id,{code:"document.body.innerHTML += \""+overlay+"\""});
+      });
+      overlayCreated = true;
+    });
+  }else{
+    if(overlayOn){
+      console.log("2");
+      overlayOn = false;
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id,{code:"$('#overlay').hide();"});
+      });
+    }else{
+      console.log("3");
+      overlayOn = true;
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id,{code:"$('#overlay').show();"});
+      });
+    }
+  }
 }
