ff53b00655fc45fbcbb27d18fa6f1cd8328e5143
Crelier <jean-claude.crelier@amersports.com>
2017-10-11 14:18:31 +0200

    popup ok

--- js/background.js
+++ js/background.js
@@ -12,25 +12,23 @@ chrome.commands.onCommand.addListener(function(command) {
 
 function toggle_digivice(){
   if(!overlayCreated){
-    console.log("1");
+    $("body").load('../pages/overlay.html');
+    var overlay = $("body").prop('outerHTML');
+    overlay = overlay.replace(/\"/g,"'").replace(/(\r\n|\n|\r)/gm,"");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      $("body").load('../pages/overlay.html');
-      var overlay = $("body").prop('outerHTML');
-      overlay = overlay.replace(/\"/g,"'").replace(/(\r\n|\n|\r)/gm,"");
-      chrome.tabs.executeScript(tabs[0].id,{file:"jquery-3.2.1.min.js"},function(){
-        chrome.tabs.executeScript(tabs[0].id,{code:"document.body.innerHTML += \""+overlay+"\""});
+      chrome.tabs.executeScript(tabs[0].id,{file:"js/jquery-3.2.1.min.js"},function(){
+        console.log("toto");
+        chrome.tabs.executeScript(tabs[0].id,{code:"$('body').append(\""+overlay+"\")"},function(){console.log(overlay);});
       });
       overlayCreated = true;
     });
   }else{
     if(overlayOn){
-      console.log("2");
       overlayOn = false;
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(tabs[0].id,{code:"$('#overlay').hide();"});
       });
     }else{
-      console.log("3");
       overlayOn = true;
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(tabs[0].id,{code:"$('#overlay').show();"});
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,36 @@
 var username = localStorage.getItem("username");
+
 if(username == null){
-    $("#welcomeMessage").hide();
+    //$("#welcomeMessage").hide();
 }else{
     $(".js_username").html(username);
+}
+$("#buttonA").click(function(){select();});
+$("#buttonB").click(function(){confirm();});
+$("#buttonC").click(function(){cancel();});
+$("body").keypress(function(key){
+  switch(key.which){
+    case 119:
+      select();
+    break;
+    case 122:
+      select();
+    break;
+    case 120:
+      confirm();
+    break;
+    case 99:
+      cancel();
+    break;
+  }
+});
+
+function select(){
+  $("#digivice").html("select");
+}
+function confirm(){
+  $("#digivice").html("confirm");
+}
+function cancel(){
+  $("#digivice").html("cancel");
 }
