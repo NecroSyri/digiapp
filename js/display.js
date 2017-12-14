1ba5e4232fdb5e2a0ed9466bc9cbf7c11a428e19
Crelier <jean-claude.crelier@amersports.com>
2017-12-14 12:39:41 +0100

    responsive sprites

--- js/display.js
+++ js/display.js
@@ -2,3 +2,24 @@
 function display(){
   $(".inscreen").html(d.mon);
 }
+
+function displayEgg(){
+    $(".inscreen").html("<div id=\"mon\"></div>");
+  $("#mon").addClass('egg');
+/*
+  $(".egg").animateSprite({
+      fps: 12,
+      animations: {
+          stop: [0],
+          shake: [0, 1],
+          pop:[0,1,2]
+      },
+      loop: true,
+      complete: function(){
+          // use complete only when you set animations with 'loop: false'
+          alert("animation End");
+      }
+  });
+  $(".egg").animateSprite("play","shake");
+  */
+}
