53b94305e62a7829b67556cb85a1233da17e61d8
Crelier <jean-claude.crelier@amersports.com>
2017-12-14 16:39:38 +0100

    animOk

--- js/display.js
+++ js/display.js
@@ -6,9 +6,35 @@ function display(){
 function displayEgg(){
     $(".inscreen").html("<div id=\"mon\"></div>");
   $("#mon").addClass('egg');
+  var myAnimation = new AM.Sprite(document.getElementById('mon'),{
+    fps:5,
+    totalFames:12,
+    columns:3,
+    rows:4
+  });
+  myAnimation.loopBetween(1,2,true);
+
+//https://www.cssscript.com/easy-sprite-animation-javascript-library-sprite-js/
+/*
+var myAnimation = new AM.Sprite(document.getElementById('demo'), {
+    totalFrames: 14,
+    columns: 5,
+    rows: 3
+});
+
+myAnimation.element.onclick = function () {
+  if (myAnimation.running) {
+    myAnimation.pause();
+  } else {
+    myAnimation.loopBetween(myAnimation.currentFrame, 0, true);
+  }
+};
+
+myAnimation.loopBetween(1, 7, true);
+*/
 /*
   $(".egg").animateSprite({
-      fps: 12,
+      fps: 0,
       animations: {
           stop: [0],
           shake: [0, 1],
@@ -21,5 +47,5 @@ function displayEgg(){
       }
   });
   $(".egg").animateSprite("play","shake");
-  */
+*/
 }
