f0f6b79e884d0feb13aa711c69df413c3cda3ed9
Crelier <jean-claude.crelier@amersports.com>
2018-01-24 17:15:29 +0100

    need to centralize d

--- js/events.js
+++ js/events.js
@@ -1,8 +1,10 @@
 function events(message){
+	load();
 	var event = message.split(",")[0];
 	var param = message.split(",")[1];
 	debug("events() - event : "+event+" param : "+param);
 	windowEvent(event,param);
+	save();
 }
 
 function resume(){
